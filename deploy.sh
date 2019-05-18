d1442c5a19f600f0f5a494bd149e0d805fb1ab9e
arsho <shovon.sylhet@gmail.com>
2019-05-18 20:53:33 +0600

    rebuilding site Sat 18 May 20:53:33 +06 2019

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+yarn build # if using a npm, 'npm run build'
+
+# Go To Build folder
+cd build
+# Add changes to git.
+git add --all
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master -f
+
+# Come Back up to the Project Root
+cd ..
