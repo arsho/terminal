430d3feb33f443981202daf9145555b04dcd98f8
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2020-07-15 11:24:05 +0600

    added menubar basic css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import TerminalHeader from './components/TerminalHeader';
+import TerminalMenu from './components/TerminalMenu';
 import TerminaBody from './components/TerminalBody';
 class App extends Component {
     render() {
         return (
             <div className="terminal">
                 <TerminalHeader/>
+                <TerminalMenu/>
                 <TerminaBody/>
             </div>
         );
